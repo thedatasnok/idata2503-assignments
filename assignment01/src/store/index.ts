2ad2fa665aa8fe980ff8d2d84f518925cd60a033
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-09-12 22:59:37 +0200

    feat: persist expenses using async storage

--- assignment01/src/store/index.ts
+++ assignment01/src/store/index.ts
@@ -1,6 +1,8 @@
 import { Expense, ExpenseType } from '@/types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { randomUUID } from 'expo-crypto';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export interface ExpenseState {
   expenses: Expense[];
@@ -13,56 +15,89 @@ export interface ExpenseActions {
 
 export type ExpenseStore = ExpenseState & ExpenseActions;
 
-// TODO: Add middleware for persisting the state in device storage
-export const useExpenseStore = create<ExpenseStore>((set, get) => ({
-  expenses: [
-    {
-      id: '1',
-      title: 'Groceries',
-      amount: 70.43,
-      date: new Date(),
-      type: ExpenseType.FOOD,
-    },
-    {
-      id: '2',
-      title: 'Movie',
-      amount: 6.5,
-      date: new Date(),
-      type: ExpenseType.LEISURE,
-    },
-    {
-      id: '3',
-      title: 'Bus ticket',
-      amount: 50.29,
-      date: new Date(),
-      type: ExpenseType.TRAVEL,
-    },
-    {
-      id: '4',
-      title: 'Lunch',
-      amount: 5.29,
-      date: new Date(),
-      type: ExpenseType.WORK,
-    },
-  ],
-  addExpense: (expense: Omit<Expense, 'id'>) => {
-    set({
+export const useExpenseStore = create<ExpenseStore>()(
+  persist(
+    (set, get) => ({
       expenses: [
-        ...get().expenses,
         {
-          ...expense,
-          id: randomUUID(),
+          id: '1',
+          title: 'Groceries',
+          amount: 70.43,
+          date: new Date(),
+          type: ExpenseType.FOOD,
+        },
+        {
+          id: '2',
+          title: 'Movie',
+          amount: 6.5,
+          date: new Date(),
+          type: ExpenseType.LEISURE,
+        },
+        {
+          id: '3',
+          title: 'Bus ticket',
+          amount: 50.29,
+          date: new Date(),
+          type: ExpenseType.TRAVEL,
+        },
+        {
+          id: '4',
+          title: 'Lunch',
+          amount: 5.29,
+          date: new Date(),
+          type: ExpenseType.WORK,
         },
       ],
-    });
-  },
-  removeExpense: (id: string) => {
-    const deletedExpense = get().expenses.find((expense) => expense.id === id);
-    if (!deletedExpense) throw new Error('Could not find expense to delete');
+      addExpense: (expense: Omit<Expense, 'id'>) => {
+        set({
+          expenses: [
+            ...get().expenses,
+            {
+              ...expense,
+              id: randomUUID(),
+            },
+          ],
+        });
+      },
+      removeExpense: (id: string) => {
+        const deletedExpense = get().expenses.find(
+          (expense) => expense.id === id
+        );
+        if (!deletedExpense)
+          throw new Error('Could not find expense to delete');
 
-    const expenses = get().expenses.filter((expense) => expense.id !== id);
-    set({ expenses });
+        const expenses = get().expenses.filter((expense) => expense.id !== id);
+        set({ expenses });
 
-    return deletedExpense;
-  },
-}));
+        return deletedExpense;
+      },
+    }),
+    {
+      name: 'expense-storage',
+      storage: {
+        getItem: async (name) => {
+          const item = await AsyncStorage.getItem(name);
+          if (!item) return null;
+
+          const { state } = JSON.parse(item);
+          const expenses: Expense[] = state.expenses.map((expense: any) => ({
+            ...expense,
+            date: new Date(expense.date),
+          }));
+
+          return {
+            state: {
+              ...state,
+              expenses,
+            },
+          };
+        },
+        setItem: async (name, value) => {
+          const item = JSON.stringify(value);
+          await AsyncStorage.setItem(name, item);
+        },
+        removeItem: (name) => AsyncStorage.removeItem(name),
+      },
+    }
+  )
+);
