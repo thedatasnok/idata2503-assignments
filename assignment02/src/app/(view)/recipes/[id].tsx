4420d6f311d8312b580d7fe569b76fbda5a60374
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-20 21:59:28 +0200

    feat: first iteration of recipe screen

--- assignment02/src/app/(view)/recipes/[id].tsx
+++ assignment02/src/app/(view)/recipes/[id].tsx
@@ -1,5 +1,13 @@
+import { getToken } from '@/config/gluestack';
 import { useFavorited, useRecipe } from '@/hooks';
-import { Icon, Pressable, styled } from '@gluestack-ui/themed';
+import {
+  Box,
+  Icon,
+  Image,
+  Pressable,
+  Text,
+  styled,
+} from '@gluestack-ui/themed';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { HeartIcon } from 'lucide-react-native';
 
@@ -29,6 +37,11 @@ const RecipeScreen = () => {
     return null;
   }
 
+  // workaround as styled does not respect the fill property if set
+  const fill = isFavorited
+    ? getToken('colors', 'error500')
+    : (getToken('colors', 'primary500') as any);
+
   return (
     <>
       <Stack.Screen
@@ -37,11 +50,42 @@ const RecipeScreen = () => {
           headerRight: () => (
             <Pressable onPress={toggleFavorited}>
               {/* @ts-ignore */}
-              <FavoriteIcon as={HeartIcon} states={{ active: isFavorited }} />
+              <FavoriteIcon
+                as={HeartIcon}
+                fill={fill}
+                states={{ active: isFavorited }}
+              />
             </Pressable>
           ),
         }}
       />
+
+      <Image
+        rounded='$md'
+        h='$48'
+        source={recipe.imageUrls[0]}
+        alt={recipe.name}
+      />
+
+      <Box alignItems='center'>
+        <Box py='$1' alignItems='center' w='$3/4'>
+          <Text fontWeight='$bold' fontSize='$xl'>
+            Ingredients
+          </Text>
+
+          <Text fontWeight='$normal' fontSize='$md' textAlign='center'>
+            {recipe.ingredients.join('\n')}
+          </Text>
+
+          <Text fontWeight='$bold' fontSize='$xl' mt='$2'>
+            Steps
+          </Text>
+
+          <Text fontWeight='$normal' fontSize='$md' textAlign='center'>
+            {recipe.steps.join('\n\n')}
+          </Text>
+        </Box>
+      </Box>
     </>
   );
 };
