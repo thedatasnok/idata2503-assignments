35d6a4fff23cf6358abdcd76cb38a621b12162c9
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-21 16:16:13 +0200

    feat: add image carousel to allow for multiple images attached to a single recipe

--- assignment02/src/app/(view)/recipes/[id].tsx
+++ assignment02/src/app/(view)/recipes/[id].tsx
@@ -11,6 +11,8 @@ import {
 } from '@gluestack-ui/themed';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { HeartIcon } from 'lucide-react-native';
+import React, { useRef, useState } from 'react';
+import { Swipeable } from 'react-native-gesture-handler';
 
 const FavoriteIcon = styled(Icon, {
   color: '$primary50',
@@ -24,14 +26,13 @@ const FavoriteIcon = styled(Icon, {
 const RecipeScreen = () => {
   const { id } = useLocalSearchParams();
   const router = useRouter();
+  const recipe = useRecipe(id as string);
+  const { isFavorited, toggleFavorited } = useFavorited(id as string);
 
   if (typeof id !== 'string') {
     router.push('/');
     return null;
   }
-
-  const recipe = useRecipe(id);
-  const { isFavorited, toggleFavorited } = useFavorited(id);
 
   if (!recipe) {
     router.push('/');
@@ -62,12 +63,7 @@ const RecipeScreen = () => {
       />
 
       <ScrollView>
-        <Image
-          rounded='$md'
-          h='$48'
-          source={recipe.imageUrls[0]}
-          alt={recipe.name}
-        />
+        <ImageCarousel images={recipe.imageUrls} recipe={recipe.name} />
 
         <Box alignItems='center'>
           <Box py='$1' alignItems='center' w='$3/4'>
@@ -89,6 +85,103 @@ const RecipeScreen = () => {
           </Box>
         </Box>
       </ScrollView>
+    </>
+  );
+};
+
+interface ImageCarouselProps {
+  images: string[];
+  recipe: string;
+}
+
+const CarouselDot = styled(Pressable, {
+  width: '$2',
+  height: '$2',
+  borderRadius: '$full',
+  bg: '$primary200',
+  ':active': {
+    bg: '$primary500',
+  },
+});
+
+/**
+ * A swipeable image carousel.
+ * Conditionally renders pressable dots if there are more than one image.
+ */
+const ImageCarousel: React.FC<ImageCarouselProps> = ({ images, recipe }) => {
+  const [index, setIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(Math.max(index - 1, 0));
+  const [nextIndex, setNextIndex] = useState(
+    Math.min(index + 1, images.length - 1)
+  );
+  const swipeableRef = useRef<Swipeable>(null);
+
+  const handleOpen = (direction: 'left' | 'right') => {
+    if (direction === 'left') {
+      setIndex(prevIndex);
+      setPrevIndex(Math.max(prevIndex - 1, 0));
+      setNextIndex(Math.min(prevIndex + 1, images.length - 1));
+    } else {
+      setIndex(nextIndex);
+      setPrevIndex(Math.max(nextIndex - 1, 0));
+      setNextIndex(Math.min(nextIndex + 1, images.length - 1));
+    }
+  };
+
+  const onDotPress = (pressedIndex: number) => {
+    if (pressedIndex < index) {
+      setPrevIndex(pressedIndex);
+      swipeableRef.current?.openLeft();
+    } else if (pressedIndex > index) {
+      setNextIndex(pressedIndex);
+      swipeableRef.current?.openRight();
+    }
+  };
+
+  return (
+    <>
+      <Swipeable
+        key={index}
+        ref={swipeableRef}
+        friction={2}
+        leftThreshold={36}
+        rightThreshold={36}
+        renderLeftActions={() => {
+          if (prevIndex === index) return undefined;
+
+          return (
+            <Image w='$full' h='$48' source={images[prevIndex]} alt={recipe} />
+          );
+        }}
+        renderRightActions={() => {
+          if (nextIndex === index) return undefined;
+
+          return (
+            <Image w='$full' h='$48' source={images[nextIndex]} alt={recipe} />
+          );
+        }}
+        onSwipeableOpen={handleOpen}
+      >
+        <Image w='$full' h='$48' source={images[index]} alt={recipe} />
+      </Swipeable>
+
+      {images.length > 1 && (
+        <Box
+          flexDirection='row'
+          alignItems='center'
+          justifyContent='center'
+          gap='$2'
+          mt='$2'
+        >
+          {images.map((_image, i) => (
+            <CarouselDot
+              key={i}
+              states={{ active: i === index }}
+              onPress={() => onDotPress(i)}
+            />
+          ))}
+        </Box>
+      )}
     </>
   );
 };
